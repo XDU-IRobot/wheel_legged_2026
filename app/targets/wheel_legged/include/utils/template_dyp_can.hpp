f0d0d0df3803f1fed47388062da2d48e3341dcb1
SF4cl <147938347+SF4cl@users.noreply.github.com>
2026-05-01 17:35:50 +0000

    Fix code style issues with clang-format

--- app/targets/wheel_legged/include/utils/template_dyp_can.hpp
+++ app/targets/wheel_legged/include/utils/template_dyp_can.hpp
@@ -17,20 +17,20 @@ class DypCanRxBridge final : public rm::device::CanDevice {
       return;
     }
 
-    distance_raw_left_   = UnpackU16BigEndian(&msg->data[0]);
-    distance_raw_right_  = UnpackU16BigEndian(&msg->data[2]);
-    last_result_left_    = msg->data[4];
-    last_result_right_   = msg->data[5];
+    distance_raw_left_ = UnpackU16BigEndian(&msg->data[0]);
+    distance_raw_right_ = UnpackU16BigEndian(&msg->data[2]);
+    last_result_left_ = msg->data[4];
+    last_result_right_ = msg->data[5];
 
     frame_count_++;
     ReportStatus(kOk);
   }
 
-  [[nodiscard]] rm::u16 distance_raw_left()  const { return distance_raw_left_; }
+  [[nodiscard]] rm::u16 distance_raw_left() const { return distance_raw_left_; }
   [[nodiscard]] rm::u16 distance_raw_right() const { return distance_raw_right_; }
-  [[nodiscard]] rm::u8  last_result_left()   const { return last_result_left_; }
-  [[nodiscard]] rm::u8  last_result_right()  const { return last_result_right_; }
-  [[nodiscard]] rm::u32 frame_count()        const { return frame_count_; }
+  [[nodiscard]] rm::u8 last_result_left() const { return last_result_left_; }
+  [[nodiscard]] rm::u8 last_result_right() const { return last_result_right_; }
+  [[nodiscard]] rm::u32 frame_count() const { return frame_count_; }
 
  private:
   static rm::u16 UnpackU16BigEndian(const rm::u8* in) {
@@ -40,7 +40,7 @@ class DypCanRxBridge final : public rm::device::CanDevice {
   std::array<rm::u8, kPayloadSize> rx_payload_{};
   rm::u16 distance_raw_left_{0U};
   rm::u16 distance_raw_right_{0U};
-  rm::u8  last_result_left_{0U};
-  rm::u8  last_result_right_{0U};
+  rm::u8 last_result_left_{0U};
+  rm::u8 last_result_right_{0U};
   rm::u32 frame_count_{0U};
 };
