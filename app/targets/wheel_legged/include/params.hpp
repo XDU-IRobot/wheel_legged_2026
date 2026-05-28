6ab8a2470b09aefd8431946cf580754b26075225
SF4cl <147938347+SF4cl@users.noreply.github.com>
2026-05-28 12:08:41 +0000

    Fix code style issues with clang-format

--- app/targets/wheel_legged/include/params.hpp
+++ app/targets/wheel_legged/include/params.hpp
@@ -1157,10 +1157,10 @@ using namespace common::main;
 
 // ── 自瞄通信 ──
 namespace aimbot {
-constexpr uint8_t kRobotId = 3U;                 ///< 机器人 ID
-constexpr float kBulletSpeedMps = 23.0f;         ///< 弹速 [m/s]
-constexpr float kBulletDefaultSpeedMps = 23.f;   ///< 默认弹速
-constexpr float kBulletBoundarySpeedMps = 20.f;  ///< 区分裁判系统返回值是否正确
+constexpr uint8_t kRobotId = 3U;                                     ///< 机器人 ID
+constexpr float kBulletSpeedMps = 23.0f;                             ///< 弹速 [m/s]
+constexpr float kBulletDefaultSpeedMps = 23.f;                       ///< 默认弹速
+constexpr float kBulletBoundarySpeedMps = 20.f;                      ///< 区分裁判系统返回值是否正确
 constexpr PidGains kYawPositionPid{80.0f, 0.f, 1.5f, 10.0f, 2.2f};   ///< 自瞄偏航位置 PID（打装甲板）
 constexpr PidGains kYawSpeedPid{0.65f, 0.0f, 0.0f, 10.0f, 0.f};      ///< 自瞄偏航速度 PID（打装甲板）
 constexpr PidGains kPitchPositionPid{40.0f, 0.f, 1.5f, 10.0f, 2.f};  ///< 自瞄俯仰位置 PID（打装甲板）
@@ -1688,10 +1688,10 @@ constexpr PidGains kYawSpeedPid{0.55f, 0.0f, 0.0f, 10.0f, 0.f};      ///< 自瞄
 constexpr PidGains kPitchPositionPid{40.0f, 0.f, 1.5f, 10.0f, 2.f};  ///< 自瞄俯仰位置 PID（打装甲板）
 constexpr PidGains kPitchSpeedPid{0.55f, 0.0f, 0.0f, 10.0f, 0.f};    ///< 自瞄俯仰速度 PID（打装甲板）
 
-constexpr PidGains kYawPositionPidRune{60.0f, 0.f, 1.5f, 10.0f, 2.2f};     ///< 自瞄偏航位置 PID（打符）
-constexpr PidGains kYawSpeedPidRune{0.55f, 0.0f, 0.0f, 10.0f, 0.f};        ///< 自瞄偏航速度 PID（打符）
+constexpr PidGains kYawPositionPidRune{60.0f, 0.f, 1.5f, 10.0f, 2.2f};   ///< 自瞄偏航位置 PID（打符）
+constexpr PidGains kYawSpeedPidRune{0.55f, 0.0f, 0.0f, 10.0f, 0.f};      ///< 自瞄偏航速度 PID（打符）
 constexpr PidGains kPitchPositionPidRune{45.0f, 0.f, 1.5f, 10.0f, 2.f};  ///< 自瞄俯仰位置 PID（打符）
-constexpr PidGains kPitchSpeedPidRune{0.55f, 0.0f, 0.0f, 10.0f, 0.f};      ///< 自瞄俯仰速度 PID（打符）
+constexpr PidGains kPitchSpeedPidRune{0.55f, 0.0f, 0.0f, 10.0f, 0.f};    ///< 自瞄俯仰速度 PID（打符）
 }  // namespace aimbot
 
 }  // namespace infantry4
